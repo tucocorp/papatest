d15102087235340b07d8b545b4e4a02aee1dbb83
Kevin <kevin.fernandez@pjchile.com>
2020-07-02 19:12:12 -0400

    Migration | Create Company

---
+++ db/migrate/20200702161231_create_companies.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateCompanies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :subdomain
+
+      t.timestamps
+    end
+  end
+end
