64296551601788ea2ee705e9139da648b3ab17e6
Kevin <kevin.fernandez@pjchile.com>
2020-07-02 19:10:38 -0400

    ApplicationController | Add before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :tenant_switcher
+
+  private
+
+  def tenant_switcher
+    tenant = request.headers['COMPANY']
+
+    if tenant.present? && Company.pluck(:subdomain).include?(tenant)
+      Apartment::Tenant.switch!(tenant)
+    else
+      render json: 'Not results found', status: 404
+    end
+  end
 end
