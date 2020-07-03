7ae9687c3a289a63b9034bc750072fb2a2be3ac8
Kevin <kevin.fernandez@pjchile.com>
2020-07-02 20:26:10 -0400

    beanstlatk.tf change ruby version

--- terraform/beanstalk.tf
+++ terraform/beanstalk.tf
@@ -6,60 +6,60 @@ resource "aws_elastic_beanstalk_application" "app" {
 resource "aws_elastic_beanstalk_environment" "production" {
   name                = "production"
   application         = "${aws_elastic_beanstalk_application.app.name}"
-  solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.6 running Ruby 2.5 (Puma) "
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.11.8 running Ruby 2.6 (Puma)"
 
   setting {
     namespace = "aws:elbv2:listener:80"
-    name = "ListenerEnabled"
-    value = "true"
+    name      = "ListenerEnabled"
+    value     = "true"
   }
 
   setting {
     namespace = "aws:elbv2:listener:80"
-    name = "Protocol"
-    value = "HTTP"
+    name      = "Protocol"
+    value     = "HTTP"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
-    name = "LoadBalancerType"
-    value = "application"
+    name      = "LoadBalancerType"
+    value     = "application"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "RAILS_ENV"
-    value = "production"
+    name      = "RAILS_ENV"
+    value     = "production"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "SECRET_KEY_BASE"
-    value = "${var.secret_key_base}"
+    name      = "SECRET_KEY_BASE"
+    value     = "${var.secret_key_base}"
   }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name = "IamInstanceProfile"
-    value = "${aws_iam_instance_profile.build.name}"
+    name      = "IamInstanceProfile"
+    value     = "${aws_iam_instance_profile.build.name}"
   }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name = "InstanceType"
-    value = "t2.micro"
+    name      = "InstanceType"
+    value     = "t2.micro"
   }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name = "EC2KeyName"
-    value = "${var.key_name}"
+    name      = "EC2KeyName"
+    value     = "${var.key_name}"
   }
 
   setting {
     namespace = "aws:autoscaling:asg"
-    name = "MinSize"
-    value = "1"
+    name      = "MinSize"
+    value     = "1"
   }
 
   setting {
@@ -76,26 +76,26 @@ resource "aws_elastic_beanstalk_environment" "production" {
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "RDS_USERNAME"
-    value = "${aws_db_instance.rds-app-prod.username}"
+    name      = "RDS_USERNAME"
+    value     = "${aws_db_instance.rds-app-prod.username}"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "RDS_PASSWORD"
-    value = "${aws_db_instance.rds-app-prod.password}"
+    name      = "RDS_PASSWORD"
+    value     = "${aws_db_instance.rds-app-prod.password}"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "RDS_DATABASE"
-    value = "${aws_db_instance.rds-app-prod.name}"
+    name      = "RDS_DATABASE"
+    value     = "${aws_db_instance.rds-app-prod.name}"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "RDS_HOSTNAME"
-    value = "${aws_db_instance.rds-app-prod.address}"
+    name      = "RDS_HOSTNAME"
+    value     = "${aws_db_instance.rds-app-prod.address}"
   }
 }
 
